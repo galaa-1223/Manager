36fc8f7fc78a4e7e39038a2be73456e82d030118
galbadrakhtserenkhand <galbadrakhtserenkhand@gmail.com>
2021-04-03 19:55:19 +0800

    4 sard huulav

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Config;
 use App\Models\Settings;
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,5 +33,21 @@ class AppServiceProvider extends ServiceProvider
                 Config::set('settings.'.$setting->key, $setting->value);
             }
         }
+
+        view()->composer('*', function ($view) 
+        {
+            if (Auth::guard('manager')->check()) {
+                $view->with('manager', Auth::guard('manager')->user());
+            }else {
+                $view->with('manager', null);
+            }
+
+            $notifications = Notification::select('activity_log.description', 'activity_log.updated_at', 'biggs.name as name', 'biggs.email as email')
+                            ->join('biggs', 'biggs.id', '=', 'activity_log.causer_id')
+                            ->limit(5)
+                            ->orderBy('updated_at', 'desc')->get();
+    
+            $view->with('notifications', $notifications );    
+        });  
     }
 }
